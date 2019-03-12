c84676d4ca9f189aedef1a7481e2ab374f39ad15
sergii.shestakov <sergii.shestakov@dev-pro.net>
2019-03-12 17:38:57 +0200

    ReplaySubject as event emitter

---
+++ 4.ts
@@ -0,0 +1,27 @@
+import { ReplaySubject } from 'rxjs';
+
+import { log, observerA, observerB } from './observers';
+
+/**
+ * --a--b--c--d|
+ * -^a--b--c--d-
+ * -------------^-a,b,c,d>
+ */
+
+const subject = new ReplaySubject(10);
+
+subject.subscribe(observerA);
+log('observerA subscribed');
+
+setTimeout(() => subject.next(1), 100);
+setTimeout(() => subject.next(2), 200);
+setTimeout(() => subject.next(3), 300);
+setTimeout(() => subject.next(3), 400);
+setTimeout(() => subject.next(3), 500);
+setTimeout(() => subject.complete(), 600);
+
+setTimeout(() => {
+    subject.subscribe(observerB);
+    log('observerB subscribed');
+
+}, 400);
