fb8e6a0c58667b89980e9f061771b79aef8b7b46
Sergey Shestakov <sshest78@gmail.com>
2019-03-12 23:03:52 +0200

    Publish and share operators in managing multicast subscriptions

---
+++ 8.ts
@@ -0,0 +1,47 @@
+import {
+    Subject, interval
+} from "rxjs/index";
+import { share, tap } from "rxjs/internal/operators";
+import { observerA, observerB } from "./observers";
+
+/**
+ * publish - combines multicast with different Subjects
+ */
+
+// publish = multicast + Subject
+// publishReplay = multicast + ReplaySubject
+// publishBehavior = multicast + BehaviorSubject
+// publishLast = multicast + AsyncSubject
+
+/**
+ * share - combines publish with refCount
+ */
+// share = publish + refCount
+// shareReplay = publishReplay + refCount
+
+const shared = interval(1000)
+    .pipe(
+        tap(() => log('source ' + x)),
+        // Subscribes on outer stream and produces inner stream through subject
+        share()
+    );
+
+
+const subA = shared.subscribe(observerA); // start stream execution (aka connect())
+
+let subB;
+setTimeout(() => {
+    subB = shared.subscribe(observerB); // 1 -> 2 subscriptions
+}, 3000);
+
+setTimeout(() => {
+    subA.unsubscribe(); // 2 -> 1
+    log('unsubscribed A');
+}, 6000);
+
+setTimeout(() => {
+    subB.unsubscribe(); // 1 -> 0; Connect unsubscribed;
+    log('unsubscribed B');
+
+}, 9000);
+
